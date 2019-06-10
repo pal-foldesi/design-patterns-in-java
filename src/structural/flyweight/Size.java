bf534d507be6c04bf50571ee76b850e3275d5a03
Pál Földesi <29228103+pal-foldesi@users.noreply.github.com>
2019-06-10 10:27:52 +0200

    Added Flyweight pattern

---
+++ src/structural/flyweight/Size.java
@@ -0,0 +1,37 @@
+package structural.flyweight;
+
+import java.util.Objects;
+
+public class Size {
+    private final double length;
+    private final double height;
+    private final String unit;
+
+    public Size(double length, double height, String unit) {
+        this.length = length;
+        this.height = height;
+        this.unit = unit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Size size = (Size) o;
+        return Double.compare(size.length, length) == 0 &&
+                Double.compare(size.height, height) == 0 &&
+                unit.equals(size.unit);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(length, height, unit);
+    }
+
+    @Override
+    public String toString() {
+        return "length=" + length +
+                ", height=" + height +
+                ", unit='" + unit + '\'';
+    }
+}
